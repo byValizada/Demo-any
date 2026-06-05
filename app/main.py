af622a66783111c7c5402e9e4cc87103265c08b5
TuralValizade <turalvalizada@mail.ru>
2026-06-05 13:03:14 +0400

    ilk yukleme

--- app/main.py
+++ app/main.py
@@ -590,7 +590,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.get_origins(),
+    allow_origins=["*"] ,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
